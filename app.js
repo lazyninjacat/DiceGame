50550aaed41534e2de191329246b8a9063e550a3
lazyninjacat <matthew.riddett@gmail.com>
2020-01-30 13:49:21 -0800

    "got the dice png displaying, and the random numbers generating properly. initial state config is close to completed.

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 1;
 
-dice = Math.floor(Math.random() * 6) + 1;
+// dice = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector("#current-" + activePlayer).textContent = dice;
+// document.querySelector("#current-" + activePlayer).textContent = dice;
 // document.querySelector("#current-" + activePlayer).innerHTML =
 //   "<em>" + dice + "</em>";
 
@@ -25,3 +25,20 @@ var x = document.querySelector("#score-0").textContent;
 console.log(x);
 
 document.querySelector(".dice").style.display = "none";
+
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  // Rondom number
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  // Display the result
+  var diceDOM = document.querySelector(".dice");
+  diceDOM.style.display = "block";
+  diceDOM.src = "dice-" + dice + ".png";
+
+  // Update the round score IF the rolled number was NOT a 1
+});
